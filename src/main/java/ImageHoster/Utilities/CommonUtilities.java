539f0c1e538fee42161062b0a93c3188bd29aa79
bhosalesantosh <bhosalesg@gmail.com>
2020-07-18 20:10:25 +0530

    Completed the utility class to validate the password

--- src/main/java/ImageHoster/Utilities/CommonUtilities.java
+++ src/main/java/ImageHoster/Utilities/CommonUtilities.java
@@ -1,9 +1,13 @@
 package ImageHoster.Utilities;
 
 import java.util.regex.*;
+
 public class CommonUtilities {
     public static boolean isValidPassword(String password) {
+        //used regular expression to validate the password as mentioned in the assignment.
+
         String regex = "^(?=.*?[A-Z,a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{3,}$";
+
         Pattern p = Pattern.compile(regex);
         if (password == null) {
             return false;
